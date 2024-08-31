5171776d43f65b7ff873cb842af67ee2b1cf10ba
vanamuthuV <vanamuthuvana22@gmail.com>
2024-08-31 11:35:24 +0530

    Bugs Fixed

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(
     origin: BaseURL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Origin", "Content-Type", "Authorization"], // Allowed headers
   })
 );
 
